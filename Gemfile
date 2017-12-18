d2cefd981bde4e0db7ba364bd1081203e1caafe0
sai243 <yskrishna2435@gmail.com>
2017-12-17 23:17:32 -0500

    signup

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ end
 
 gem 'jquery-rails'
 gem 'rails', '~> 5.1.4'
+gem 'bcrypt', '~> 3.1.7' 
 gem 'puma', '~> 3.7'
 gem 'sass-rails', '~> 5.0'
 gem 'simple_form', '~> 3.5'
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171214142834) do
+ActiveRecord::Schema.define(version: 20171215163013) do
 
   create_table "articles", force: :cascade do |t|
-    t.string        "title"
-    t.text          "description"
-    t.datetime      "created_at"
-    t.datetime      "updated_at"
-    t.integer       "user_id"
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20171214142834) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end

5f87cd4d5bc5084bae342c8725742317871d6b87
sai243 <yskrishna2435@gmail.com>
2017-12-27 15:45:40 -0500

    e

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:sucess] = "Welcome to the alpha blog #{@user.username}"
+      flash[:success] = "Welcome to the alpha blog #{@user.username}"
       redirect_to articles_path
     else
       render 'new'

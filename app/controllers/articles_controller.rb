3bb15a6b4db892cd0c5761c89a91622dd627d3e5
sai243 <yskrishna2435@gmail.com>
2017-12-27 15:06:34 -0500

    authentication added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
-    before_action :set_article, only:[:edit, :update, :show, :destroy]
+    before_action :set_article, only: [:edit, :update, :show, :destroy]
+    before_action :require_user, except: [:index, :show]
+    before_action :require_same_user, only: [:edit, :update, :destroy]
 
 
   def index
@@ -24,7 +26,8 @@ class ArticlesController < ApplicationController
 
   def create
   @article = Article.new(article_params)
-  @article.user = User.first
+  #@article.user = User.first
+  @article.user = current_user
     if @article.save
      flash[:success] ="Article was sucessfully created!"
      redirect_to  article_path(@article)
@@ -51,6 +54,12 @@ class ArticlesController < ApplicationController
 
   def article_params
     params.require(:article).permit(:title, :description)
+  end
+  def require_same_user
+    if current_user != @article.user
+       flash[:danger] = "you can only edit or delete your own articles "
+       redirect_to root_path
+    end
   end
 
 end
